6e91589e8637eee7be944df4c55d96099e2fb763
wepeqoor1 <hoopeeeee@gmail.com>
2022-06-09 14:21:57 +0300

    first_commit

---
+++ read_data_from_excel.py
@@ -0,0 +1,30 @@
+import collections
+
+import pandas as pd
+from pprint import pformat
+
+excel_data_df: pd.DataFrame = pd.read_excel(
+    'wine2.xlsx',
+    sheet_name='Лист1',
+    na_values=None,
+    keep_default_na=False,
+)
+
+uniq_categories: list = excel_data_df['Категория'].unique()
+
+wine_categories: collections.defaultdict = collections.defaultdict(
+    list,
+    {
+        category: excel_data_df[excel_data_df['Категория'].isin([category])].to_dict(
+            orient='records'
+        )
+        for category in uniq_categories
+    },
+
+)
+
+# print(pformat(wine_categories))
+
+for wine_category in wine_categories:
+    print(wine_category)
+    print(wine_categories[wine_category])
---
+++ word_parsing.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+
+def year_with_article(year: int) -> str:
+
+    if year in range(5, 20) or \
+            year % 10 == 0 or \
+            year % 10 in range(5, 20) or \
+            year % 100 in range(5, 20):
+        return f'{year} лет'
+    if year % 10 in range(2, 5):
+        return f'{year} года'
+    return f'{year} год'
+
+
+YEAR_OF_CREATION_WINERY: int = 1920
+current_year: int = datetime.now().year
+winery_age = current_year - YEAR_OF_CREATION_WINERY
+current_year_winery = year_with_article(year=winery_age)
