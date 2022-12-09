e0b58cd5a5465609cf68982f9f0ccab771dbf342
wepeqoor1 <hoopeeeee@gmail.com>
2022-12-10 01:59:53 +0300

    Parse excel file to defaultdict

--- read_data_from_excel.py
+++ read_data_from_excel.py
@@ -1,24 +1,26 @@
 import collections
 
 import pandas as pd
-from pprint import pformat
 
-excel_data_df: pd.DataFrame = pd.read_excel(
-    'wine2.xlsx',
-    sheet_name='Лист1',
-    na_values=None,
-    keep_default_na=False,
-)
 
-uniq_categories: list = excel_data_df['Категория'].unique()
+def get_assortment_wines() -> collections.defaultdict:
+    excel_data_df: pd.DataFrame = pd.read_excel(
+        'wine2.xlsx',
+        sheet_name='Лист1',
+        na_values=None,
+        keep_default_na=False,
+    )
 
-wine_categories: collections.defaultdict = collections.defaultdict(
-    list,
-    {
-        category: excel_data_df[excel_data_df['Категория'].isin([category])].to_dict(
-            orient='records'
-        )
-        for category in uniq_categories
-    },
+    uniq_categories: list = excel_data_df['Категория'].unique()
 
-)
+    wine_categories = collections.defaultdict(
+        list,
+        {
+            category: excel_data_df[excel_data_df['Категория'].isin([category])].to_dict(
+                orient='records'
+            )
+            for category in uniq_categories
+        },
+    )
+    return wine_categories
+
