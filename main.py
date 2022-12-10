a9bc93cdf41e8e9bbc043f76b55e79804c966855
wepeqoor1 <hoopeeeee@gmail.com>
2022-12-10 03:02:06 +0300

    Fix code be pythonic

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from year_word_declension import current_year_winery
 from read_data_from_excel import get_assortment_wines
 
+from exceptions import ToManyXlsxFilesException
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
-
-rendered_page = template.render(
-    current_year_winery=current_year_winery,
-    wines_assortment=get_assortment_wines(),
-)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        current_year_winery=current_year_winery,
+        wines_assortment=get_assortment_wines(),
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 # 127.0.0.1:8000/index.html
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except ToManyXlsxFilesException:
+        print('Файл формата ".xlsx" должен быть равен одному')
