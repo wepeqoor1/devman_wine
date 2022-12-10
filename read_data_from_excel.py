bd4b8537b8f3f7ad146150eec24e61991f67fa07
wepeqoor1 <hoopeeeee@gmail.com>
2022-12-10 03:00:08 +0300

    Check number files on main directory

--- read_data_from_excel.py
+++ read_data_from_excel.py
@@ -1,11 +1,26 @@
 import collections
+import os
 
 import pandas as pd
 
+from exceptions import ToManyXlsxFilesException
+
 
 def get_assortment_wines() -> collections.defaultdict:
+    """
+    Из .xlsx файла считывает данные и преобразует в массив collections.defaultdict
+    """
+    files = os.listdir()
+    xlsx_files = []
+    for file in files:
+        if '.xlsx' in file:
+            xlsx_files.append(file)
+    if len(xlsx_files) != 1:
+        raise ToManyXlsxFilesException
+
+    xlsx_file = xlsx_files[0]
     excel_data_df: pd.DataFrame = pd.read_excel(
-        'wine2.xlsx',
+        xlsx_file,
         sheet_name='Лист1',
         na_values=None,
         keep_default_na=False,
@@ -23,4 +38,3 @@ def get_assortment_wines() -> collections.defaultdict:
         },
     )
     return wine_categories
-
