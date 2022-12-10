7873eeb048d8a0e03655733bf43fef1186b1b0cf
wepeqoor1 <hoopeeeee@gmail.com>
2022-12-10 13:37:44 +0300

    Fix file structure

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-from year_word_declension import current_year_winery
 from read_data_from_excel import get_assortment_wines
 
 from exceptions import ToManyXlsxFilesException
+from year_word_declension import year_with_article
 
 
 def main():
@@ -16,7 +16,7 @@ def main():
         )
         template = env.get_template('template.html')
         rendered_page = template.render(
-            current_year_winery=current_year_winery,
+            current_year_winery=year_with_article(),
             wines_assortment=get_assortment_wines(),
         )
         with open('index.html', 'w', encoding="utf8") as file:
--- year_word_declension.py
+++ year_word_declension.py
@@ -4,7 +4,9 @@ from datetime import datetime
 YEAR_OF_CREATION_WINERY: int = 1920
 
 
-def year_with_article(year: int) -> str:
+def year_with_article() -> str:
+    current_year: int = datetime.now().year
+    winery_age = current_year - YEAR_OF_CREATION_WINERY
     """
     Возвращает слово "год" в зависимости от количества лет
     Пример:
@@ -12,16 +14,11 @@ def year_with_article(year: int) -> str:
         5 лет
         103 года
     """
-    if year in range(5, 20) or \
-            year % 10 == 0 or \
-            year % 10 in range(5, 20) or \
-            year % 100 in range(5, 20):
-        return f'{year} лет'
-    if year % 10 in range(2, 5):
-        return f'{year} года'
-    return f'{year} год'
-
-
-current_year: int = datetime.now().year
-winery_age = current_year - YEAR_OF_CREATION_WINERY
-current_year_winery = year_with_article(year=winery_age)
+    if winery_age in range(5, 20) or \
+            winery_age % 10 == 0 or \
+            winery_age % 10 in range(5, 20) or \
+            winery_age % 100 in range(5, 20):
+        return f'{winery_age} лет'
+    if winery_age % 10 in range(2, 5):
+        return f'{winery_age} года'
+    return f'{winery_age} год'
