3f63a3b475636c0636fc2174df144f5c187eeac7
wepeqoor1 <hoopeeeee@gmail.com>
2022-12-12 23:47:47 +0300

    Remove extra excepton

--- exceptions.py
+++
@@ -1,2 +0,0 @@
-class ToManyXlsxFilesException(BaseException):
-    pass
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from read_data_from_excel import get_assortment_wines
 
-from exceptions import ToManyXlsxFilesException
 from year_word_declension import get_year_with_article
 
 
@@ -30,23 +29,21 @@ def get_console_arguments():
 def main():
     args = get_console_arguments()
     xlsx_filename = args.file
-    try:
-        env = Environment(
-            loader=FileSystemLoader('.'),
-            autoescape=select_autoescape(['html', 'xml'])
-        )
-        template = env.get_template('template.html')
-        rendered_page = template.render(
-            year_with_article=get_year_with_article(),
-            wines_assortment=get_assortment_wines(xlsx_filename),
-        )
-        with open('index.html', 'w', encoding="utf8") as file:
-            file.write(rendered_page)
 
-        server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-        server.serve_forever()
-    except ToManyXlsxFilesException:
-        print('Файл формата ".xlsx" должен быть равен одному')
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        year_with_article=get_year_with_article(),
+        wines_assortment=get_assortment_wines(xlsx_filename),
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
 
 if __name__ == '__main__':
